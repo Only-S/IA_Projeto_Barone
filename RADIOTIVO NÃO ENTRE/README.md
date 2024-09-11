6ee54f43e739c2d68269804508dee1a436cbb90a
Only-S <richards1305@gmail.com>
2024-09-11 09:39:07 -0300

    Atualização repositorio

--- RADIOTIVO NÃO ENTRE/README.md
+++
@@ -1,2 +0,0 @@
-# IA_Projeto_Barone
-Projeto de IA baseado em casos, com a finalidade de recomendar jogos com base nas perguntas respondidas pelo usuário.
--- RADIOTIVO NÃO ENTRE/calculo_vizinhanca/calc3.0/db_itens.py
+++
@@ -1,6 +0,0 @@
-# Base de dados invertidas - Exemplo com jogos
-from conexao_banco.service.CasosService import processar_casos
-
-casos_jogos = processar_casos()
-
-print(casos_jogos)
